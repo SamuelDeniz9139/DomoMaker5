3019d530a16c2a3560079ef5644e0fe029b85263
Sam <sfd9139@g.rit.edu>
2022-04-03 19:51:25 -0400

    changed domo info formatting and updated new feature

--- client/maker.jsx
+++ client/maker.jsx
@@ -7,9 +7,11 @@ const handleDomo=(e)=>{
     const color=e.target.querySelector('#domoColor').value;
     const _csrf=e.target.querySelector('#_csrf').value;
     if(!name&&!age&&!color){
-        name="lazy";
-        age="42"
-        color="white, i guess"
+        let noName="I wasn't named.";
+        let noAge="42"
+        let noColor="White, I guess"
+        helper.sendPost(e.target.action, {noName,noAge,noColor,_csrf}, loadDomosFromServer);
+        return false;
     } else if(!name||!age||!color){
         helper.handleError('All fields are required.');
         return false;
@@ -45,9 +47,7 @@ const DomoList=(props)=>{
         return(
             <div key={domo._id} className="domo">
                 <img src="/assets/img/domoface.jpeg" alt="domo face" className="domoFace" />
-                <h3 className="domoName"> Name: {domo.name} </h3>
-                <h3 className="domoAge"> Age: {domo.age} </h3>
-                <h3 className="domoColor"> Color: {domo.color} </h3>
+                <h3 className="domoInfo"> {domo.name}, the {domo.age}-year old {domo.color} Domo. </h3>
             </div>
         );
     });
